f701dfcf05aa7b961ab3103ebaa393e963fca99a
Lin <mt5718214@gmail.com>
2020-11-21 16:24:31 +0800

    feat: add create function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,6 +4,21 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const name = req.body.name
+  const UserId = req.user.id
+  Todo.create({
+    name,
+    UserId
+  })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
 router.get('/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
